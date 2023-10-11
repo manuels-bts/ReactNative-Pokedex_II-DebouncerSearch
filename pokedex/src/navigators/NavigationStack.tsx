e6498d031057ef88d26c0c3bb0bb5434d04cb2d5
Manuel Salinas <manuelsalinas.mx@outlook.com>
2023-10-11 14:44:40 -0600

    git init

---
+++ pokedex/src/navigators/NavigationStack.tsx
@@ -0,0 +1,31 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import { HomeScreen } from "../screens/HomeScreen";
+import { PokemonScreen } from "../screens/PokemonScreen";
+import { SimplePokemon } from "../interfaces/pokemonInterfaces";
+import { SearchScreen } from '../screens/SearchScreen';
+
+
+export type RootStackParams = {
+    HomeScreen: undefined;
+    PokemonScreen: SimplePokemon;
+    SearchScreen: undefined;
+}
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const NavigationStack = () => {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false, // Ocultar el navigation bar
+                cardStyle: {
+                    backgroundColor: 'white'
+                }
+            }}
+
+        >
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
+        </Stack.Navigator>
+    );
+}
